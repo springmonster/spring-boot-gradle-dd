96a8d3fc6003d3554611cc8740084ca6862551d0
kuanghc1 <kuanghc1@lenovo.com>
2021-12-10 10:25:30 +0800

    init

---
+++ src/main/java/com/example/springbootgradledd/SpringBootGradleDdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgradledd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleDdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGradleDdApplication.class, args);
+    }
+
+}
